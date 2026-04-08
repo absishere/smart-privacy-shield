c57ab59bc6492e832d2b738fe2c6fea181168ecd
RugvedSapkal <sapkalrugved9@gmail.com>
2026-04-08 11:59:28 +0530

    feat: deploy contracts to Sepolia testnet

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -9,6 +10,10 @@ module.exports = {
       url: "http://127.0.0.1:7545",
       chainId: 1337,
       // Hardhat will automatically use the first account provided by Ganache
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.ADMIN_PRIVATE_KEY],
     },
   },
 };
